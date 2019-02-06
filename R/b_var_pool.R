b56bc331f4cc5679d50095d1dcdf1f7aecc3409d
Johannes <fritz.johannes@gmail.com>
2019-02-06 17:36:26 +0100

    new param

--- R/b_var_pool.R
+++ R/b_var_pool.R
@@ -11,13 +11,15 @@
 
 # Function infos and parameters  --------------------------------------------
 
-b_var_pool <- function(x, y) {
+b_var_pool <- function(x, y, uncommon.selection="random") {
   nr.vars=round(mean(c(length(x), length(y))),0)
 
   common.vars=intersect(x,y)
   uncommon.vars=c(x,y)[c(x,y)%in% common.vars==F]
 
-  uncommon.choice=sample(1:length(uncommon.vars),nr.vars-length(common.vars), replace=F)
+  if(uncommon.selection=="random"){
+    uncommon.choice=sample(1:length(uncommon.vars),nr.vars-length(common.vars), replace=F)
+  }
 
   return(c(common.vars,uncommon.vars[uncommon.choice]))
 }
