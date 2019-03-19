1661d30ea1c21662ef7d7bc96506d4a05c898dd5
Johannes <fritz.johannes@gmail.com>
2019-03-19 11:03:15 +0100

    dotname

--- R/b_create_model_variables.R
+++ R/b_create_model_variables.R
@@ -390,7 +390,7 @@ b_create_model_variables <- function(bid=NULL,
     aggregate.variables$acting.agency=acting.agency
     aggregate.variables$acting.agency=as.factor(aggregate.variables$acting.agency)
     aa.dummies = as.data.frame(predict(dummyVars(~ acting.agency, data = aggregate.variables), newdata = aggregate.variables))
-    names(aa.dummies)=tolower(gsub(" ","-",gsub("acting.agency","",names(aa.dummies))))
+    names(aa.dummies)=tolower(gsub(" ","-",gsub("acting.agency\\.?","",names(aa.dummies))))
     aa.dummies$bid=bid
 
 
