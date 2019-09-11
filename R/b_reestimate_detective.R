f326aa3edffead2f8811ad81add1893019b8fc9c
Johannes <fritz.johannes@gmail.com>
2019-09-11 15:09:24 +0200

    more choices.

--- R/b_reestimate_detective.R
+++ R/b_reestimate_detective.R
@@ -11,7 +11,9 @@
 
 b_reestimate_detective = function(detective=NULL,
                                   estimation.turns=10,
-                                  train.share=.82
+                                  train.share=.82,
+                                  save.result=T,
+                                  save.classifier=T
                                   ){
   load("content/0 core/Classifier statistics & history.Rdata")
 
@@ -24,6 +26,12 @@ b_reestimate_detective = function(detective=NULL,
   dt.incl=subset(model, name==detective & detective.no==d.no)$dtm.incl
   dt.mtrc=subset(model, name==detective & detective.no==d.no)$dtm.metric
   nr.of.terms=subset(model, name==detective & detective.no==d.no)$dtm.terms
+
+  ## old stats
+  old.scores=subset(stats, detective.no==d.no)$score
+  old.scores.adjusted=subset(stats, detective.no==d.no)$score.adjusted
+  old.scores=old.scores[length(old.scores)]
+  old.scores.adjusted=old.scores.adjusted[length(old.scores.adjusted)]
 
   ## train and save
   training.stats=b_train_detective(detective.number=d.no,
@@ -41,22 +49,25 @@ b_reestimate_detective = function(detective=NULL,
   t.stats$score=score_me(t.stats$capture, t.stats$reduction)
   t.stats$score.adjusted=score_me(t.stats$capture.adjusted, t.stats$reduction.adjusted)
 
-  old.scores=subset(stats, detective.no==d.no)$score
-  old.scores.adjusted=subset(stats, detective.no==d.no)$score.adjusted
-  old.scores=old.scores[length(old.scores)]
-  old.scores.adjusted=old.scores.adjusted[length(old.scores.adjusted)]
-
   print(paste("old vs new score: ", round(max(old.scores, old.scores.adjusted),2)," vs ",round(max(t.stats$score, t.stats$score.adjusted),2),sep=""))
 
   ## storing result
-  load("content/0 core/Classifier statistics & history.Rdata")
 
-  stats=rbind(stats, t.stats)
-  word.score=training.stats$word.scores
-  incumbent.classifier=training.stats$classifier
+  if(save.result){
+    load("content/0 core/Classifier statistics & history.Rdata")
 
-  save(model, stats, word.score, file="content/0 core/Classifier statistics & history.Rdata")
-  save(incumbent.classifier, file=paste("content/0 core/",Sys.Date()," - ",detective," classifier.Rdata", sep=""))
+    stats=rbind(stats, t.stats)
+    word.score=training.stats$word.scores
+    incumbent.classifier=training.stats$classifier
+
+    save(model, stats, word.score, file="content/0 core/Classifier statistics & history.Rdata")
+
+  }
+
+  if(save.classifier){
+    save(incumbent.classifier, file=paste("content/0 core/",Sys.Date()," - ",detective," classifier.Rdata", sep=""))
+  }
+
 
   print(paste("Finish",detective))
 
