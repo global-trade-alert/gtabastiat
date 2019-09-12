3e893bf75562f0c6a4b8e93889b85bb1203fd121
Johannes <fritz.johannes@gmail.com>
2019-09-12 11:22:21 +0200

    reset the estimate detective function

---
+++ R/bt_estimate_detective.R
@@ -0,0 +1,55 @@
+# Roxygen documentation
+
+#' Bastiat, please re-estimate this detective.
+#'
+#' @return Several data frames about how the detective did.
+#' @references www.globaltradealert.org
+#' @author Johannes Fritz for GTA
+
+
+# Function infos and parameters  --------------------------------------------
+
+bt_estimate_detective = function(detective.name=NULL,
+                                 robustness.turns=0,
+                                 train.share=.82,
+                                 save.stats=T,
+                                 save.classifier=T,
+                                 training.data="data/classifier/training data.Rdata"
+                                 ){
+
+
+  for(detective in detective.name){
+    print(paste("Starting",detective))
+
+    detective.characteristics=bt_get_detective_characteristics(detective.name=detective)
+
+    load("data/classifier/training data.Rdata")
+
+    detective.data=bt_create_estimation_data(bid=training$bid,
+                                          evaluation=training$evaluation,
+                                          text=training$text,
+                                          train.share = train.share,
+                                          variables=detective.characteristics$variables,
+                                          dtm.incl=detective.characteristics$dtmatrix.included,
+                                          dtm.metric=detective.characteristics$dtmatrix.metric,
+                                          dtm.terms=detective.characteristics$dtmatrix.term.count,
+                                          acting.agency=detective.characteristics$vars.incl.acting.agency,
+                                          act.values=detective.characteristics$vars.incl.act.value,
+                                          is.td=detective.characteristics$vars.incl.td,
+                                          keywords=detective.characteristics$vars.incl.keywords)
+
+    detective.classifier=bt_estimate_classifier(training.data=detective.data$estimation.data,
+                                             training.id="bid",
+                                             training.y="evaluation",
+                                             estimation.model=detective.characteristics$estimation.model,
+                                             robustness.turns=robustness.turns,
+                                             train.share=train.share,
+                                             update.log=save.stats,
+                                             save.classifier=save.classifier,
+                                             detective.name=detective.characteristics$detective.name)
+
+
+    print(paste("Finish",detective))
+  }
+}
+
