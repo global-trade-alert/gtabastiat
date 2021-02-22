48d9038e0c28cc24da379e08b897624e197c8a7c
ccampb17 <ccampb17@mail.bbk.ac.uk>
2021-02-22 14:51:29 +0000

    added bahasa back in

--- R/bt_guess_month.R
+++ R/bt_guess_month.R
@@ -31,12 +31,12 @@ bt_guess_month = function(tgt.string){
   #nb upper/lowercase allowances, and replacement of any diacritics with regex '.' (diacritics often cause corruption)
 
 
-  #save(german, french, spanish, russian, portuguese, italian, arabic.gregorian, file="R help files/month_names.Rdata")
+  #save(german, french, spanish, russian, portuguese, italian, bahasa, arabic.gregorian, file="R help files/month_names.Rdata")
   load(file="R help files/month_names.Rdata")
 
   Encoding(russian) <- "UTF-8" #required for russian to work
 
-  months.master = paste(german, french, spanish, russian, portuguese, italian, arabic.gregorian, sep = ")|(")
+  months.master = paste(german, french, spanish, russian, portuguese, italian, arabic.gregorian, bahasa, sep = ")|(")
 
   months.master = paste0("(", months.master, ")")
 
