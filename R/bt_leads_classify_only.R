5fdf2023f29d8faa7962439e7599f914b390abe6
ccampb17 <ccampb17@mail.bbk.ac.uk>
2021-05-07 11:49:31 +0100

    now can merge non-classified and classified properly

--- R/bt_leads_classify_only.R
+++ R/bt_leads_classify_only.R
@@ -50,6 +50,8 @@ bt_leads_classify_only = function(update.core){
     classified.relevance=classify$relevant
     classified.rel.prob=round(classify$relevance.probability,4)
 
+    lc.update$relevance.probability = 0
+
     lc.update=rbind(subset(lc.update, ! bid %in% classified.bids),
                     classify)
 
