8a21da22ab79c6ec81ba2139925199be784d8ceb
ccampb17 <ccampb17@mail.bbk.ac.uk>
2021-05-17 17:00:43 +0100

    added functionality to save a col of the match words

--- R/bt_update_core_kw_filter.R
+++ R/bt_update_core_kw_filter.R
@@ -7,6 +7,7 @@
 #' @param positive if F, entries containing the key.words will be discarded.
 #' @param filter.cols which columns to use for filtering
 #' @param only.keep.matches if True, sets all relevant/classify to 0 first
+#' @param save.match.words if True, add a column "key.words" containing all the words that generated matches
 #'
 #' @return filtered dataframe
 #' @export
@@ -17,9 +18,12 @@ bt_update_core_kw_filter = function(update.core,
                                     positive = T,
                                     filter.cols = c("act.title.en", "act.description.en"),
                                     case.sensitive = F,
-                                    only.keep.matches=T){
+                                    only.keep.matches=T,
+                                    save.match.words = T){
 
   filtered = update.core
+
+  filtered$match.words = NA
 
   if(positive & only.keep.matches){
     filtered$relevant = 0
@@ -36,11 +40,25 @@ bt_update_core_kw_filter = function(update.core,
 
       if(positive){
         filtered$relevant[match.vect] <- 1 -> filtered$classify[match.vect]
+
+        #save the word for context/meta-analysis
+
+        #create a vector to index rows that matched and do NOT already contain the word
+        add.words.vect = (match.vect & !grepl(filtered$match.words, word))
+        filtered$match.words[add.words.vect] = paste(filtered$match.words[add.words.vect], word, sep = ", ")
+
+
       }else{
         filtered$relevant[match.vect] <- 0 -> filtered$classify[match.vect]
       }
     }
 
+  }
+
+  setNames(filtered, "match.words", "key.words")
+
+  if(!save.match.words){
+    filtered$key.words = NULL
   }
 
   return(filtered)
