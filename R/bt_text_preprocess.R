43bc9cdd1cea53f9e36a4d3778252b6d01ef0726
Callum Campbell <callum.campbell@sgept.org>
2021-12-15 14:01:28 +0000

    added ftfy to remove mojibake (which are a serious issue)
    stopword removal optional with new param

--- R/bt_text_preprocess.R
+++ R/bt_text_preprocess.R
@@ -4,12 +4,12 @@
 #'
 #'
 #' @param text string or character vector
-#'
+#' @param stop.rm remove stopwords. set to F for e.g. acting.agency
 #' @return
 #' @export
 #'
 #' @examples
-bt_text_preprocess = function(text){
+bt_text_preprocess = function(text, stop.rm = T){
 
   library(tm)
 
@@ -19,6 +19,16 @@ bt_text_preprocess = function(text){
   #          removeWords(tm::stopwords(kind = "en")) %>%
   #          gsub(pattern = "\\s+", replacement = " ")
   # )
+
+  library(reticulate)
+
+  ftfy=reticulate::import("ftfy")
+
+  #must be a loop as ftfy can't do vectors afaik
+  print("fixing mojibake...")
+  for(i in 1:length(text)){
+    text[i] = ftfy$fix_text(text[i])
+  }
 
   return(text %>%
            gsub(pattern = "< ?br[\\s\\S]+", replacement = "", perl = T) %>%
