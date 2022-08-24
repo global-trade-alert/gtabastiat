59ed8df8356d7b253382e9ba364684959706bfa7
Callum Campbell <callum.campbell@sgept.org>
2022-08-24 13:34:26 +0100

    added increased timeout, and check that the file uploads correctly

--- R/bt_upload_to_aws.R
+++ R/bt_upload_to_aws.R
@@ -14,13 +14,34 @@ bt_upload_to_aws = function(upload.file.name=NULL,
                             upload.file.path=NULL,
                             upload.destination="gtaupload/Uploads/files/",
                             credential.path="setup/keys/as3.R") {
+
+  library(httr)
+  library(glue)
+
   ## For how to interact with AWS from the command line, see https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
   source(credential.path)
   upload.location=gsub("/+","/",paste0(upload.file.path,"/",upload.file.name))
   upload.location=gsub("^/+","",upload.location)
-  system(paste0(aws.cred," aws s3 cp '",upload.location,"' s3://",upload.destination," --acl public-read"))
+  system(paste0(aws.cred," aws s3 cp '",upload.location,"' s3://",upload.destination," --acl public-read --cli-connect-timeout 6000"),timeout = 60)
   aws.url=paste0("http://s3-eu-west-1.amazonaws.com/",gsub("/+","/",paste0(upload.destination,"/",upload.file.name)))
 
-  return(aws.url)
+  #check the upload worked correctly
+  r = tryCatch({
+    httr::HEAD(url=aws.url, timeout(5))
+  }, error = function(e) {
+    FALSE
+  })
+
+
+  if(is.logical(r) | r$status_code!=200){
+    stop(glue("Error uploading to AWS! The URL was supposed to be: {aws.url}"))
+  }
+
+
+
+  if(r$status_code == 200){
+    print("AWS upload return code 200!")
+    return(aws.url)
+  }
 }
 
